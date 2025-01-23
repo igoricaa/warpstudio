4e2c0381808ce5a4c77c444d324c732a3e23665f
Igor Stanisavljevic <stanisavljevic.igor@proton.me>
2025-01-23 11:50:11 +0100

    logo to jsx, header desktop

--- components/header.tsx
+++ components/header.tsx
@@ -1,26 +1,20 @@
-import Image from 'next/image';
 import Link from 'next/link';
 import { ThemeSwitcher } from './theme-switcher';
+import Logo from './ui/icons/logo';
+import MenuItem from './menu-item';
 
 const Header = () => {
   return (
     <header className='absolute top-0 w-screen px-side h-40 flex justify-between items-center '>
-      <div className='relative w-52 aspect-[51/14]'>
-        <Image
-          // src='/warp-logo.png'
-          src='/logo-black.svg'
-          alt='Warp Logo'
-          fill
-          // className='object-cover'
-        />
+      <div className='relative'>
+        <Link href='/'>
+          <Logo className='w-52 h-auto' />
+        </Link>
       </div>
-      {/* bg-clip-text text-transparent */}
-      <div className='flex items-center gap-10 text-foreground text-3xl font-medium bg-[linear-gradient(93deg,#C84159_0%,_#6697D0_100%)] '>
-        <Link href='/'>Home</Link>
-        <Link href='/contact'>Contact</Link>
-        <div className='flex w-fit'>
-          <ThemeSwitcher />
-        </div>
+
+      <div className='flex items-center gap-10 text-foreground text-3xl font-medium bg-clip-text relative before:content-[""] before:absolute before:-bottom-2 before:left-1/2 before:-translate-x-1/2 before:w-[calc(100%+1.25rem)] before:h-[2px] before:bg-[linear-gradient(93deg,#C84159_0%,_#6697D0_100%)] before:z-10'>
+        <MenuItem />
+        <ThemeSwitcher />
       </div>
     </header>
   );
---
+++ components/menu-item.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+const MenuItem = () => {
+  const pathname = usePathname();
+
+  return (
+    <Link
+      href={pathname === '/contact' ? '/' : '/contact'}
+      className='uppercase'
+    >
+      {pathname === '/contact' ? 'Home' : 'Contact'}
+    </Link>
+  );
+};
+
+export default MenuItem;
