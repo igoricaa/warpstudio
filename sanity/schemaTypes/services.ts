b4a850d14c28dc161a34cf12c074ba7acc5527f0
Igor Stanisavljevic <stanisavljevic.igor@proton.me>
2025-02-09 11:50:48 +0100

    services tweak schema

--- sanity/schemaTypes/services.ts
+++ sanity/schemaTypes/services.ts
@@ -6,6 +6,12 @@ export default defineType({
   type: 'document',
   fields: [
     defineField({
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
       name: 'services',
       title: 'Services',
       type: 'array',
